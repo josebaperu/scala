c101212f13694dbde707f9a39996e272dccf910a
joseTG <josetorresgarrido@gmail.com>
2024-06-25 15:49:18 -0500

    base commit

---
+++ src/test/scala/com/example/ScalaTest.scala
@@ -0,0 +1,9 @@
+package com.example
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class ScalaTest extends AnyFunSuite {
+  test("CubeCalculator.cube") {
+    assert("HELLO".toLowerCase() === "hello")
+  }
+}
