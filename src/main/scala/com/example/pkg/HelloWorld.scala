8c598fef2fd778606cc9319a5484ab4c144149be
joseTG <josetorresgarrido@gmail.com>
2024-09-01 01:28:08 -0500

    HelloWorld

--- src/main/scala/com/example/pkg/HelloWorld.scala
+++ src/main/scala/com/example/pkg/HelloWorld.scala
@@ -5,6 +5,24 @@ object HelloWorld {
   def main (args: Array[String]) {
     val world = "world"
     val year = 2024
-    println(s"hello ${world}: ${year}")
+    println(s"hello $world: $year")
+
+
+    for(i <- 1.to(5)){
+      println(s"loop $i")
+    }
+    for (i <- 6.until(11)) {
+      println(s"loop $i")
+    }
+
+    val list = List(9,8,7,6,5)
+    for(i <- list){
+      println(s"loop $i")
+    }
+
+    val result = for {i <- list;if i > 6} yield {
+      i*2
+    }
+    println(result)
   }
 }
